e31c7087c62aafd72a8172e59d1de4b12583c9a9
MAngeles <mariangeles.torres.medina@gmail.com>
2021-01-03 16:48:37 +0100

    toolbar

--- app/src/main/java/com/example/appeasyshop/MainActivity.java
+++ app/src/main/java/com/example/appeasyshop/MainActivity.java
@@ -22,14 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -47,7 +40,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_pedido) {
+            return true;
+        }
+
+        if (id == R.id.action_recetas) {
             return true;
         }
 
